aa3a1dd3f9573918649d77259e7ac16d349876e8
Vardan Jaswal <vardanthakur94@gmail.com>
2024-10-08 21:17:42 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,1 +1,16 @@
+import Link from 'next/link';
 
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Student Information</h1>
+      <p>Name: Vardan </p>
+      <p>
+        GitHub Repository: 
+        <Link href="https://github.com/Vardan07-VJ/cprg306-assignments" passHref>
+          <a target="_blank">My GitHub Repo</a>
+        </Link>
+      </p>
+    </main>
+  );
+}
