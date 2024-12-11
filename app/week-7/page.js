a05d1e2e3fce41ca3aa4d1051cbe3f6f87877780
Vardan Jaswal <vardanthakur94@gmail.com>
2024-12-11 06:27:40 +0530

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,1 +1,21 @@
-
+"use client"; 
+import React, { useState } from 'react';
+import ItemList from './item-list';
+import NewItem from './new-item'; 
+import itemsData from './items.json';
+const Page = () => {
+  const [items, setItems] = useState(itemsData);
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+  return (
+    <main className="min-h-screen bg-black-20 p-3">
+      <div className="max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold text-center text-white-700 mb-6">Shopping List</h1>
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items} />
+      </div>
+    </main>
+  );
+};
+export default Page;
