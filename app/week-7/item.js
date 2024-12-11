0c3d2db4f7e15ab36cc85196c7a373c5e60a6f5a
Vardan Jaswal <vardanthakur94@gmail.com>
2024-12-11 06:26:36 +0530

    Update item.js

--- app/week-7/item.js
+++ app/week-7/item.js
@@ -1,1 +1,13 @@
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="flex justify-between items-center p-3 bg-gray-800 rounded-md shadow mb-2">
+      <div className="flex flex-col">
+        <span className="text-lg font-bold text-gray-50">{name}</span>
+        <span className="text-sm text-gray-400">Category: {category}</span>
+      </div>
+      <span className="text-sm text-gray-400">Qty: {quantity}</span>
+    </li>
+  );
+};
 
+export default Item;
